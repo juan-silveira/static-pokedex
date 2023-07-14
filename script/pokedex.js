50057b41b60e74c4aa6a546ad88401e2226c42d7
Juan Silveira <juansilveira@gmail.com>
2023-07-14 12:06:21 -0300

    feat: fix responsivity at memory game

--- script/pokedex.js
+++ script/pokedex.js
@@ -64,7 +64,7 @@ const fetchApi = async (pkmnName) => {
 }
 
 const fetchPkmnList = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=905&offset=0');
   
     if (response.status === 200) {
       const pkmnData = await response.json();
